ada2931591e8d622d4ace2b56b4a23f214822fc8
race4space <58293087+race4space@users.noreply.github.com>
2020-04-25 19:11:00 +0200

    xxx

--- src/XDesign.php
+++ src/XDesign.php
@@ -18,6 +18,8 @@ class XDesign{
     $this->obj_pdo=new \phplibrary\PDO;
     $this->obj_pdo->fn_connect($con_host, $con_user, $con_pass, $con_schema);
     //*/
+
+    $this->bln_debug=true;
   }
   function fn_execute() {
 
@@ -27,7 +29,71 @@ class XDesign{
     global $obj_publish;
 
     $obj_request=new \phplibrary\ServerVariables();
-    $obj_xpublish_const=new XPublishConstant();
+    //$obj_xpublish_const=new XPublishConstant();  
 
+    //fn_write_post();
+
+    //echo "SUCCESSABC";
+
+//*
+$str_json= file_get_contents("php://input");
+//var_dump($str_json);
+echo(PHP_EOL);
+$obj_post = json_decode($str_json);
+$this->obj_post=$obj_post;
+//$arr_myArray = json_decode($str_json, true);
+
+//var_dump($obj_myObject);
+
+$str_action=$obj_post->Action;
+$this->str_data="";
+if(isset($obj_post->Data)){
+  $this->str_data=$obj_post->Data;
+}
+
+/*
+echo("str_action: ".$str_action);
+echo(PHP_EOL);
+echo("$this->str_data: ".$this->str_data);
+echo(PHP_EOL);
+//*/
+
+    switch($str_action){
+      case "saveDesignerToServer":
+        $this->fn_saveDesignerToServer();
+      break;
+      case "loadDesignerFromServer":
+        $this->fn_loadDesignerFromServer();
+      break;      
+      default:
+        echo "XDesign ACTION Not Handled: [".$str_action."]";
+    }
+  }
+  function fn_loadDesignerFromServer(){    
+
+    $str_sql="SELECT * FROM `designer` LIMIT 1;";//siteItem    
+    $stmt = $this->obj_pdo->pdo->query($str_sql);
+    $row=$stmt->fetch();
+    if($row){
+      //var_dump($row);    
+      $str_data=$row["Serialize"];
+      echo($str_data);
+    }
+  }
+  function fn_saveDesignerToServer(){
+
+    $str_data=$this->str_data;
+    $str_data=$this->obj_pdo->pdo->quote($str_data);
+
+    $str_sql="DELETE FROM `designer` LIMIT 1;";//siteItem    
+    $stmt = $this->obj_pdo->pdo->query($str_sql);
+    
+    $str_sql="INSERT INTO `designer` (`serialize`) VALUES ($str_data);";
+    $stmt = $this->obj_pdo->pdo->query($str_sql);    
+
+    if($this->bln_debug){
+      echo("fn_saveDesignerToServer: SUCCESS");
+    }
+  }
 }//END CLASS XDesign
 ?>
